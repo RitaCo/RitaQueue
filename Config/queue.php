77e27880174aa05be51d267902a186ef19e4a417
zoghal <saleh.souzanchi@gmail.com>
2014-05-11 14:08:15 +0430

    update config

--- Config/queue.php
+++ Config/queue.php
@@ -7,7 +7,7 @@ $config['Queue'] = array(
 	'defaultworkerretries' => 4,
 	'workermaxruntime' => 0,
 	'cleanuptimeout' => 2000,
-	'exitwhennothingtodo' => false,
+	'exitwhennothingtodo' => true,
 	'pidfilepath' => TMP . 'queue' . DS,
 	'log' => true,
 	'notify' => 'tmp' // Set to false to disable (tmp = file in TMP dir)
